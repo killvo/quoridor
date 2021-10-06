b15d8ee0cf683a67b92267b580e0fc7effddc1fa
volyk_evheniy <evhenii_official@gmail.com>
2021-10-06 14:45:23 +0300

    created game initializer domain

---
+++ backend/src/main/java/com/quebec/core/domains/initializer/GameInitController.java
@@ -0,0 +1,34 @@
+package com.quebec.core.domains.initializer;
+
+import com.quebec.core.domains.initializer.dto.GameStartWithBotResponse;
+import com.quebec.core.domains.initializer.dto.GameStartWithPlayersResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("init")
+public class GameInitController {
+    private final GameInitService gameInitService;
+
+    @Autowired
+    public GameInitController(GameInitService gameInitService) {
+        this.gameInitService = gameInitService;
+    }
+
+    @PostMapping("start_two_people")
+    public GameStartWithPlayersResponse start2people() {
+        return gameInitService.startTwoPeople();
+    }
+
+    @PostMapping("start_with_bot")
+    public GameStartWithBotResponse startWithBot() {
+        return gameInitService.startWithBot();
+    }
+
+    @PostMapping("reset")
+    public boolean resetGame() {
+        return gameInitService.resetGame();
+    }
+}
