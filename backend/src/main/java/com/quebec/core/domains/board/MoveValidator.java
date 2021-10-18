6643a6445b689ccf18d39112a3ebaded6efa09ec
IllyaPashchenko <illya.pashchenko@gmail.com>
2021-10-18 17:41:29 +0300

    Added move validation

--- backend/src/main/java/com/quebec/core/domains/board/MoveValidator.java
+++ backend/src/main/java/com/quebec/core/domains/board/MoveValidator.java
@@ -3,11 +3,11 @@ package com.quebec.core.domains.board;
 import com.quebec.core.domains.move.dto.MakeMoveRequest;
 import com.quebec.core.domains.move.dto.PlaceWallRequest;
 import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
 import org.jgrapht.graph.DefaultEdge;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @Component
 public class MoveValidator {
@@ -18,7 +18,18 @@ public class MoveValidator {
     }
 
     public boolean isMovePlayerValid(Graph<String, DefaultEdge> board, MakeMoveRequest request, Map<UUID, String> playerPositions) {
-        //TODO: Add validation
+        String activePlayerPosition = playerPositions.get(request.getId());
+        String targetPosition = request.getXCorner() + "" + request.getYCorner();
+
+        if(playerPositions.containsValue(targetPosition)) return false;
+        if(board.containsEdge(activePlayerPosition, targetPosition)) return true;
+
+        List<String> activeNeighbours = Graphs.neighborListOf(board, activePlayerPosition);
+        List<String> targetNeighbours = Graphs.neighborListOf(board, targetPosition);
+        activeNeighbours.retainAll(targetNeighbours);
+        for (String neighbour: activeNeighbours) {
+            if (playerPositions.containsValue(neighbour)) return true;
+        }
         return false;
     }
 }
